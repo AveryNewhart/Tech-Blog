83d26cc77fdd7dfc252ae0fc2d80c988906970a3
AveryNewhart <newhartreeceavery@gmail.com>
2023-02-18 20:45:01 -0500

    added more files, reformat

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+const path = require('path');
 const express = require('express');
-const path = require('path')
 const session = require('express-session');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const routes = require('./controller');
+const timeHelper = require('./utils/time')
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const hbs = exphbs.create({ helpers });
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ timeHelper });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -29,9 +37,13 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-}); 
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} to view application!`
+    )
+  );
+});
