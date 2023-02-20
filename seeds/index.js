4ce5f1992fbd29ceeba0320c2515def5153279ef
AveryNewhart <newhartreeceavery@gmail.com>
2023-02-19 21:32:51 -0500

    redid seed files

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,23 @@
-const sequelize = require("../config/connection");
-const seedUser = require('./preLoadUsers');
-const seedBlogs = require('./preLoadBlogs')
+const sequelize = require('../config/connection');
+const { User, Blogs } = require('../models');
 
+const userData = require('./preLoadUser.json');
+const blogData = require('./preLoadBlogs.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await seedUser();
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  await seedBlogs()
+  for (const blog of blogData) {
+    await Blogs.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
